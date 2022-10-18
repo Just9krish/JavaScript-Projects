c298a3d541252a2d377062bccc9495bd8b599d09
just9krish <just9krish434@gamil.com>
2022-10-18 20:12:51 +0530

    completed

--- Tip  Calculator/js/app.js
+++ Tip  Calculator/js/app.js
@@ -2,17 +2,56 @@ const formElements = document.querySelector("form");
 
 formElements.addEventListener("submit", function (e) {
   e.preventDefault();
-  const billAmount = Number(document.getElementById("input-bill").value);
-  const numberOfCustomer = Number(document.getElementById("input-users").value);
-  const serviceValue = Number(document.getElementById("input-service").value);
 
+  let billAmount = Number(document.getElementById("input-bill").value);
+  let numberOfCustomer = Number(document.getElementById("input-users").value);
+  let serviceValue = Number(document.getElementById("input-service").value);
+
+  const isFeedback = validateForm(billAmount, numberOfCustomer);
+
+  if (!isFeedback) {
+    const loader = document.querySelector(".loader");
+    const resultsElements = document.querySelector(".results");
+
+    loader.classList.add("showItem");
+    const result = calculateTip(billAmount, numberOfCustomer, serviceValue);
+
+    setTimeout(function () {
+      loader.classList.remove("showItem");
+
+      document.getElementById("tip-amount").textContent = `${result[0].toFixed(
+        2
+      )}`;
+      document.getElementById(
+        "total-amount"
+      ).textContent = `${result[1].toFixed(2)}`;
+      document.getElementById(
+        "person-amount"
+      ).textContent = `${result[2].toFixed(2)}`;
+      resultsElements.classList.add("showItem");
+    }, 2000);
+  }
+});
+
+const reset = document.querySelector(".resetBtn");
+
+reset.addEventListener("click", function () {
+  document.getElementById("input-bill").value = parseInt("0");
+  document.getElementById("input-users").value = parseInt("0");
+  document.querySelector(".results").classList.remove("showItem");
+});
+
+function validateForm(billAmount, numberOfCustomer) {
   const feedback = document.querySelector(".feedback");
+
+  let isFeedback = false;
 
   if (billAmount == "" || billAmount <= 0) {
     feedback.classList.add("showItem", "alert-danger");
     let pElement = document.createElement("p");
     pElement.innerHTML = "Bill amount cannot be blank.";
     feedback.appendChild(pElement);
+    isFeedback = true;
   }
 
   if (numberOfCustomer <= 0) {
@@ -20,17 +59,17 @@ formElements.addEventListener("submit", function (e) {
     let pElement = document.createElement("p");
     pElement.innerHTML = "Number of customer must be more than zero.";
     feedback.appendChild(pElement);
+    isFeedback = true;
   }
 
   setTimeout(() => {
     feedback.classList.remove("showItem", "alert-danger");
   }, 10000);
 
-  calculateTip(billAmount, numberOfCustomer, serviceValue)
-});
+  return isFeedback;
+}
 
 function calculateTip(billAmount, numberOfCustomer, selectedService) {
-
   let percentageTip = 0;
 
   if (selectedService == 1) {
@@ -45,7 +84,5 @@ function calculateTip(billAmount, numberOfCustomer, selectedService) {
   const totalAmount = billAmount + tipAmount;
   const eachPerson = totalAmount / numberOfCustomer;
 
-  document.getElementById("tip-amount").textContent = tipAmount;
-  document.getElementById("total-amount").textContent = totalAmount;
-  document.getElementById("person-amount").textContent = eachPerson;
+  return [tipAmount, totalAmount, eachPerson];
 }
